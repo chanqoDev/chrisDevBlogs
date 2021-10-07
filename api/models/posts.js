9a82b9e1b1740e7711543af3bfddde3bb9597460
Christian E <christianelena92@gmail.com>
2021-10-07 16:33:59 -0500

    Create an API Endpoint to return a post based on id

--- api/models/posts.js
+++ api/models/posts.js
@@ -7,18 +7,30 @@ class Post {
     return this.readData();
   }
 
-  getIndividualBlog() {
+  getIndividualBlog(postId) {
     // get one Blog post
+    const posts = this.readData();
+    const foundPost = posts.find((post) => post.id == postId);
+    return foundPost;
   }
 
-  add() {
+  add(newPost) {
     // add new post
+    const currentPosts = this.readData();
+    currentPosts.unshift(newPost);
+    this.storeData(currentPost);
   }
+
   readData() {
     // get data form data.json
     let rawdata = fs.readFileSync(PATH);
     let posts = JSON.parse(rawdata);
     return posts;
+  }
+
+  storeData(rawData) {
+    let data = JSON.stringify(rawData);
+    fs.writeFileSync(PATH, data);
   }
 }
 
